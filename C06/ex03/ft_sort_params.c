a6666e04a911b7562d8582c1385f81a92f567f9b
Aldisti <76822748+Aldisti@users.noreply.github.com>
2022-09-23 15:14:09 +0200

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adi-stef <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 18:37:18 by adi-stef          #+#    #+#             */
+/*   Updated: 2022/09/21 15:35:44 by adi-stef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] - s2[i] == 0)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_print(char **argv, int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (++i < n)
+	{
+		j = -1;
+		while (argv[i][++j])
+			write(1, &argv[i][j], 1);
+		write(1, "\n", 1);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	*temp;
+	int		i;
+	int		j;
+
+	if (argc <= 1)
+		return (0);
+	i = 0;
+	while (++i < argc)
+	{
+		j = i;
+		while (++j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+		}
+	}
+	ft_print(argv, argc);
+}
